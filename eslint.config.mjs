4d7dcec1497fe1eefabbdd470c9f4fd2c5745a25
HUANG Cheng <cheng@duck.com>
2025-09-02 09:38:32 +0800

    chore: add linting configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint-disable-next-line unicorn/import-style
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import unicorn from 'eslint-plugin-unicorn';
+
+import { recommended } from '@cspell/eslint-plugin/configs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,16 +15,32 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+  //...compat.extends('next/core-web-vitals', 'next/typescript'),
+  unicorn.configs.recommended,
+  // jsdoc.configs['flat/recommended-typescript-error'],
+  recommended,
+  ...compat.config({
+    extends: [
+      'next',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'plugin:import/react',
     ],
-  },
+    plugins: ['prettier', 'import'],
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+      'unicorn/no-abusive-eslint-disable': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-null': 'off',
+    },
+  }),
 ];
 
 export default eslintConfig;
